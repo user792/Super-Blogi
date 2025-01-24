c154998de2f076ba4fc66109c64dfc47464ad526
JhuuJS <joonatan.surakka@edu.hyria.fi>
2025-01-24 13:02:00 +0200

    Update index.php
    tämä hetki

--- De Blog/php/index.php
+++ De Blog/php/index.php
@@ -1,13 +1,28 @@
 <?php include_once 'inc/header.php'; ?>
     <div class="kysely">
         <h1>Kirjaudu Sisään</h1>
-        <form action="actions/signin.php" method="post">
+        <form action="./actions/signin.php" method="post">
 
             
             <div class="container">
                 
                 <h2>Tervettuloa ja kirjaudu</h2>
                 <p>saat SMB päivityksiä</p>
+    <?php
+    if(isset($_GET['err'])){
+        $error = $_GET['err'];
+        if($error == 'fieldempty'){
+            echo"<p style?'color:red;'>Täytä kaikki kentät</p>";
+        }else if($error == 'usernull'){
+            echo"<p style='color:red;'>Käyttäjää ei ole olemassa</p>";
+        }else if($error == 'stmtfailed'){
+            echo"<p style='color:red;'>Virhe</p>";
+        }else if($error == 'wrongpass'){
+            echo"<p style='color:red;'>Väärät kirjautumistiedot</p>";
+        }
+    
+    }
+    ?>
                 <label for="user_name"><b>Käyttäjänimi</b></label>
                 <input type="text" class="textbox" placeholder="Käyttäjänimi" name="user_username" required>
                 <br>
