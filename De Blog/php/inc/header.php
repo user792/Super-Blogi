3acd6044aebc0d5dabd604f4b2a2988d397c4853
JhuuJS <joonatan.surakka@edu.hyria.fi>
2025-01-24 09:05:43 +0200

    Update header.php
    uusi stylesheet lokaatio

--- De Blog/php/inc/header.php
+++ De Blog/php/inc/header.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Account</title>
+    <link rel="stylesheet" href="../CSS/signin.css">
     <style>
         body {
             font-family: Arial, sans-serif;
