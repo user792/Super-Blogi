821ffd6309db3bad0c6dd33e81d6a479d6e41103
JhuuJS <joonatan.surakka@edu.hyria.fi>
2025-01-24 13:01:27 +0200

    Update SignUp.php
    erroreita

--- De Blog/php/actions/SignUp.php
+++ De Blog/php/actions/SignUp.php
@@ -3,14 +3,25 @@
 
 
 if(isset($_POST['signup'])){
-    $username = $_POST['user_username'];
-    $password = $_POST['user_password'];
+    $usersname = $_POST['user_username'];
+    $pssword = $_POST['user_password'];
     
+    require_once "../inc/dbinc.php";
+    include_once "./functions/functions.php";
 
+    if(signupFieldEmpty($usersname, $pssword) !== false){
+        header("Location: ../newaccount.php?err=fieldempty");
+        exit();
+    }
 
+    if(usersnameExist($conn, $usersname)){
+        header("Location: .../newAccount.php?err=username-exists");
+        exit();
+    }
 
+    createAccount($conn, $usersname, $pssword);
 }else{
     header("Location: ../index.php");
-    exit()
+    exit();
 }
 ?>
