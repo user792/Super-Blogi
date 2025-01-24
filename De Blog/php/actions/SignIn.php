b4c337e09938662717c423c350ac4611f37ccc68
JhuuJS <joonatan.surakka@edu.hyria.fi>
2025-01-24 13:01:06 +0200

    Update SignIn.php
    erroreita

--- De Blog/php/actions/SignIn.php
+++ De Blog/php/actions/SignIn.php
@@ -1,20 +1,24 @@
-<?php 
+<?php
+
 
 
 if(isset($_POST['signin'])){
-
     $usersname = $_POST['user_username'];
     $pssword = $_POST['user_password'];
-
-
-
     
+    require_once "../inc/dbinc.php";
+    include_once "./functions/functions.php";
+
+    if(signupFieldEmpty($usersname, $pssword) !== false){
+        header("Location: ../newaccount.php?err=fieldempty");
+        exit();
+    }
+
+
+
     login($conn, $usersname, $pssword);
-    
-
 }else{
     header("Location: ../index.php");
     exit();
 }
-
 ?>
