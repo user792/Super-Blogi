cc2446447f18451af9f0d63edfce70b5635d5659
JhuuJS <joonatan.surakka@edu.hyria.fi>
2025-01-24 13:02:45 +0200

    Update newaccount.php
    tämä hetki

--- De Blog/php/newaccount.php
+++ De Blog/php/newaccount.php
@@ -1,14 +1,26 @@
 <?php include_once 'inc/header.php'; ?>
     <div class="kysely">
         <h1>Tee account</h1>
-        <form action="/actions/signup" method="post">
+        <form action="./actions/signup.php" method="post">
 
     
             
             <div class="container">
                 <p>Saa SMB päivityksiä</p>
                 <h2>Tee käyttäjä</h2>
-
+        <?php
+        if(isset($_GET['err'])){
+            $error = $_GET['err'];
+            if($error == 'fieldempty'){
+                echo"<p style?'color:red;'>Täytä kaikki kentät</p>";
+            }else if($error == 'username-exists'){
+                echo"<p style='color:red;'>Käyttäjä nimi on jo käytössä</p>";
+            }else if($error == 'stmtfailed'){
+                echo"<p style='color:red;'>Virhe</p>";
+            }
+        
+        }
+        ?>
                 
                 
                 <label for="user_password"><b>Käyttäjänimi</b></label>
