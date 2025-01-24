43b9e57cfc491f9aa9bba83bf21e406b6cb173ba
JhuuJS <joonatan.surakka@edu.hyria.fi>
2025-01-24 09:03:42 +0200

    Update index.php
    yhdistelty signin

--- De Blog/php/index.php
+++ De Blog/php/index.php
@@ -1,35 +1,26 @@
 <?php include_once 'inc/header.php'; ?>
     <h1>Kirjaudu Sisään</h1>
     <form action="actions/signin.php" method="post">
-        <?php
-        
 
-
-        ?>
-        <div class="imgcontainer">
-
-        </div>
         
         <div class="container">
             
-            <h2>Welcome, Login</h2>
-            <p>and get SMB updates</p>
-            <label for="uname"><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="Uname" required>
+            <h2>Tervettuloa ja kirjaudu</h2>
+            <p>saat SMB päivityksiä</p>
+            <label for="user_name"><b>Käyttäjänimi</b></label>
+            <input type="text" class="textbox" placeholder="Käyttäjänimi" name="user_username" required>
             <br>
-            <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required>
+            <label for="user_password"><b>Salasana</b></label>
+            <input type="password" class="textbox" placeholder="Enter Password" name="user_password" required>
             <br>
-            <button type="submit">Login</button>
-            <label>
-                <input type="checkbox" checked="checked" name="remember"> Remember me
-            </label>
+            <button type="submit">Kirjaudu</button>
+
 
         </div>
 
-        <div class="container" style="background-color: #f1f1f1;">
-
-            <span class="psw">Forgot <a href="newaccount.php">password</a>?</span>
+        <div class="container" id="bottomshit">
+                <button type="button" class="cancelbtn"><a href="./Home.php">peruuta</a></button>
+                <span class="psw"><a href="newaccount.php">Unohtuiko</a>?</span>
         </div>
     </form>
 <?php include_once 'inc/footer.php'; ?>
