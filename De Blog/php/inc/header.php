aeea44e62c0def9334f04c278fdacf2d2b347b12
JhuuJS <joonatan.surakka@edu.hyria.fi>
2025-01-24 08:48:17 +0200

    Add files via upload

---
+++ De Blog/php/inc/header.php
@@ -0,0 +1,15 @@
+<?php require_once 'inc/dbinc.php'; ?>
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <link rel="icon" href="../Markoblog.png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+    </style>
+</head>
+<body>
