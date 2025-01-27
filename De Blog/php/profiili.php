622ff8e535e54a7615b1eebc17b0d938f5e52614
user792 <miikka.tallus@edu.hyria.fi>
2025-01-27 11:45:52 +0200

    Add files via upload

---
+++ De Blog/php/profiili.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../CSS/profiili.css">
+</head>
+<body>
+        <img class="kuva" src="PP/54.png" >
+    <h1>You is da one that accessed this site without proper authorization</h1>
+</body>
+</html>
+<?php
+
+
+?>
